7085853133eabb863b1d1237742cf26f82e8bdb7
Trm-xnh <lamthanh51124@gmail.com>
2025-12-18 18:19:02 +0700

    D

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import JSONBig from "json-bigint";
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "import.meta.env.VITE_API_URL",
   headers: { "Content-Type": "application/json" },
   transformResponse: [(data) => {
     try {
